46fd33ee1b9403dfe3b4a184382d68a3e90c68d4
HichamBENKADA <hicham20014@gmail.com>
2024-01-31 10:31:48 -0500

    Products Table React application environment is created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import products from "./components/data"
+import './App.css'
+import FilterableProductTable from './components/FilterableProductTable'
+
+function App() {
+ 
+
+  return (
+    <>
+    <FilterableProductTable />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/FilterableProductTable.jsx
@@ -0,0 +1,7 @@
+export default function FilterableProductTable(props){
+    return (
+        <>
+        Hi...
+        </>
+    )
+}
